107d5c8859794be967f2a75a9804e715de7adf87
wk633 <wk633@outlook.com>
2017-12-09 18:28:47 -0800

    project init(eslint, editorconfig, gitignore)

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": ["eslint:recommended"],
+  "parser": "babel-eslint",
+  "rules": {
+   "no-console": ["error", {
+     "allow": ["warn", "error", "info"]
+   }]
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "script"
+  },
+  // "globals": {
+  //   "window": true
+  // },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "mocha": true
+  }
+};
